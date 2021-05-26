24801a2131ee3292f5ea01e60ae11219077e1cb2
Taylor Lightbourne <79942688+taylorlightbourne@users.noreply.github.com>
2021-05-26 17:18:20 -0400

    functionality and styling complete

--- public/script.js
+++ public/script.js
@@ -1,61 +1,88 @@
-const list = document.getElementById("list");
-const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
-const LINE_THROUGH =  "lingThrough";
+const container = document.querySelector('.container');
+const inputValue = document.querySelector('.input');
+const add = document.querySelector('.add');
 
-function addToDo( toDo, id, done, trash ){
-
-    if ( trash ){ return; }
-    const DONE = done ? CHECK: UNCHECK;
-    const LINE = done ? LINE_THROUGH : "";
-
-    const text = <li class="item">
-                    <i class="fa ${DONE} complete" job="complete" id="${id}"></i>
-                    <p class="text ${LINE}"> ${toDo} </p>
-                    <i class="de fa fa-trash-o" job="delete" id="${id}"></i>
-                </li>
-
-    const position = "beforeend";
-
-    list.insertAdjacentHTML(position,text);
+if(window.localStorage.getItem("todos") == undefined){
+     const todos = [];
+     window.localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-addToDo("Drink Coffee");
+const todosEX = window.localStorage.getItem("todos");
+const todos = JSON.parse(todosEX);
 
-const input = document.getElementById("task-input");
-document.addEventListener("keyup", function(event){ 
-    if( event.keyCode == 13) {
-        const toDo = input.value;
-        if(toDo) {
-            addToDo(toDo, id, false, false);
-            LIST.push(
-                {name: toDo,
-                id: id,
-                done: false,
-                trash:false
-                }
-            )
-        }
-        input.value = "";
-        id++;
+
+class item{
+	constructor(name){
+		this.createItem(name);
+	}
+    createItem(name){
+    	const itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+    	const input = document.createElement('input');
+    	input.type = "text";
+    	input.disabled = true;
+    	input.value = name;
+    	input.classList.add('item_input');
+
+    	const edit = document.createElement('button');
+    	edit.classList.add('edit');
+    	edit.innerHTML = "";
+    	edit.addEventListener('click', () => this.edit(input, name));
+
+    	const remove = document.createElement('button');
+    	remove.classList.add('remove');
+    	remove.innerHTML = "";
+    	remove.addEventListener('click', () => this.remove(itemBox, name));
+
+    	container.appendChild(itemBox);
+
+        itemBox.appendChild(input);
+        itemBox.appendChild(edit);
+        itemBox.appendChild(remove);
+
     }
-});
 
-let LIST = [];
-let id = 0;
-LIST = [{} ,{} , ... ];
+    edit(input, name){
+        if(input.disabled == true){
+           input.disabled = !input.disabled;
+        }
+    	else{
+            input.disabled = !input.disabled;
+            let indexof = todos.indexOf(name);
+            todos[indexof] = input.value;
+            window.localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }
 
-LIST[0] ->
-{
-    name: "Drink Coffee",
-    id: 0,
-    done : false,
-    trash : false
-} 
-LIST[1] ->
-{
-    name: "Workout",
-    id: 1,
-    done : true,
-    trash : false
-} 
+    remove(itemBox, name){
+        itemBox.parentNode.removeChild(itemBox);
+        let index = todos.indexOf(name);
+        todos.splice(index, 1);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
+}
+
+add.addEventListener('click', check);
+window.addEventListener('keydown', (e) => {
+	if(e.which == 13){
+		check();
+	}
+})
+
+function check(){
+	if(inputValue.value != ""){
+		new item(inputValue.value);
+        todos.push(inputValue.value);
+        window.localStorage.setItem("todos", JSON.stringify(todos));
+		inputValue.value = "";
+	}
+}
+
+
+for (const v = 0 ; v < todos.length ; v++){
+    new item(todos[v]);
+}
+
+
+new item("Create a ToDo List");
